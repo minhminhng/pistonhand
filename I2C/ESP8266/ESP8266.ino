4e65b358be424c97ddd121c322d23bace5d4091d
Minh Nguyen <minhnguyen93@outlook.com>
2018-08-26 12:21:28 +0300

    volatile control

--- I2C/ESP8266/ESP8266.ino
+++ I2C/ESP8266/ESP8266.ino
@@ -1,19 +1,14 @@
 /*
- Basic ESP8266 MQTT example
-
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
+### ESP8266 MQTT
 
  It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
+  
+  - subscribes to the topic "control", printing out any messages
     it receives. NB - it assumes the received payloads are strings not binary
     
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
+  - If the first character of the topic "control" is an which is sent to control Arduino
 
- It will reconnect to the server if the connect
- ion is lost using a blocking
+ It will reconnect to the server if the connection is lost using a blocking
  reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
  achieve the same result without blocking the main loop.
 
@@ -22,6 +17,14 @@
        http://arduino.esp8266.com/stable/package_esp8266com_index.json
   - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
   - Select your ESP8266 in "Tools -> Board"
+
+### Serial data from ESP can be observed from another Arduino with bypassing the bootloader of ATM328
+    ESP   |   Uno
+    Rx    -   Rx
+    Tx    -   Tx
+              Reset  - GND
+More information can be found at https://tttapa.github.io/Pages/Arduino/ESP8266/Flashing/Flashing-With-an-Arduino.html
+    
 
 */
 
@@ -33,13 +36,14 @@
 #include <ArduinoJson.h>
 
 // Network parameters
-const char* ssid = "MinhN";
-const char* password = "Minna123";
-
+//const char* ssid = "MinhN";
+//const char* password = "Minna123";
+const char* ssid = "aalto open";
+const char* password = "";
 
 // MQTT service
-const char* mqtt_server = "192.168.0.103";
-//const char* mqtt_server = "10.100.30.1";
+//const char* mqtt_server = "192.168.0.103";
+const char* mqtt_server = "10.100.31.1";
 
 const char* mqtt_user = "minhminh";
 const char* mqtt_password = "mosquitto";
@@ -48,21 +52,20 @@ const char* mqtt_password = "mosquitto";
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
-char msg[100];
 
-unsigned int pos = 0;
-unsigned int dir = 0;
-unsigned int s1 = 0;
-unsigned int s2 = 0;
+unsigned int pos = 0;   // position of the hand
+unsigned int dir = 0;   // moving direction - closing or opening
+unsigned int s1 = 0;    // value from sensor 1
+unsigned int s2 = 0;    // value from sensor 2
+unsigned int object = 0;// presence of the object
 
-
-char *encode_to_json(){ 
-  const size_t bufferSize = JSON_OBJECT_SIZE(3);
+void encodeToJSON(){ 
+  const size_t bufferSize = JSON_ARRAY_SIZE(2) + JSON_OBJECT_SIZE(4);
   DynamicJsonBuffer jsonBuffer(bufferSize);
   JsonObject & JSONencoder = jsonBuffer.createObject();
-  JSONencoder["position"] = pos;
-  if (dir == 0){
-    JSONencoder["status"] = "stop";
+  JSONencoder["position"] = pos;        // position of the piston
+  if (dir == 0){                        // the status of the hand - stop, closing or opening
+    JSONencoder["status"] = "stop";     
   }
   else if(dir == 1){
     JSONencoder["status"] = "closing";
@@ -71,37 +74,45 @@ char *encode_to_json(){
     JSONencoder["status"] = "opening";
   }
 
-  
-  JsonArray& temperature = JSONencoder.createNestedArray("pressures");
+  JsonArray& temperature = JSONencoder.createNestedArray("pressures");  // pressures from the sensors
   temperature.add(s1);
   temperature.add(s2);
-  
-  //JSONencoder.printTo(Serial);
-  char JSONmessage[100];
-  JSONencoder.printTo(JSONmessage, sizeof(JSONmessage));
-//  if (client.publish("planting_room/data/module001", JSONmessage) == true){
-//    Serial.println(" Success sending message");
-//  }
-//  else {
-//    Serial.println(" Error senging message");
-//  }
-  return JSONmessage;
+
+  if (object == 0){
+    JSONencoder["object"] = "absent";
+  }
+  else if (object == 1){
+    JSONencoder["object"] = "present";
+  }
+
+  // Econding data to Json message
+  char data[200];
+  JSONencoder.printTo(data, sizeof(data)); // encode to Json
+  if (client.publish("data", data) == true){  // publish the message containing data to MQTT sever
+        Serial.println("Success");
+  }
+  else {
+      Serial.println("Error");
+  }
 }
 
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
-  Serial.begin(115200);
-  Wire.begin(0,2);
-  Wire.setClockStretchLimit(40000);
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
+  //pinMode(BUILTIN_LED, OUTPUT);       // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);                 // Initialize serial data for monitoring
+  Wire.begin(0,2);                      // Initialize i2c connection Wite.begin(int sda, int sck), ESP8266 only functions as master
+  Wire.setClockStretchLimit(40000);     // Increase the waiting time to solve problem of 
+                                        // slow Arduino 
+                                        // https://forum.arduino.cc/index.php?topic=336543.0
+                                        // http://www.digole.com/forum.php?topicID=777
+  setupWifi();
+  client.setServer(mqtt_server, 1883);  // Initialize mqtt service
   client.setCallback(callback);
 }
 
-void setup_wifi() {
+void setupWifi() {
 
   delay(10);
-  // We start by connecting to a WiFi network
+  // Start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -119,78 +130,7 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void i2cConnect(){
-  Wire.beginTransmission(I2CAddressESPWifi);
-  Wire.write(2);
-  Wire.endTransmission();
-  delay(1);
-  int ava = Wire.requestFrom(I2CAddressESPWifi, 7);
-  Serial.print("Request Return:[");
-  unsigned int i = 0;
-  byte msg[7] = {0};
-  if (ava == 7){
-    while(Wire.available()){
-      byte c = Wire.read();
-      msg[i++] = c;
-      Serial.print(c);
-      Serial.print(" ");
-    }
-    
-    s1 = (msg[0] << 8) | msg[1];
-    Serial.println(s1);
-    s2 = (msg[2] << 8) | msg[3];
-    Serial.println(s2);
-    
-    dir = msg[4];
-    Serial.println(dir); 
-    pos = (msg[5] << 8) | msg[6];
-    Serial.println(pos);
-  }
-  else {
-    Serial.println("Unexpected number of bytes receiced.");
-  }
-  
-  Serial.println("]");
-  delay(500);
-
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  if(strcmp(topic, "test_connection") == 0){
-    Serial.print("Message from server: ");
-//    Serial.print(topic);
-//    Serial.print("] ");
-//    for (int i = 0; i < length; i++) {
-//      Serial.print(payload[i]);
-//    }
-    if (payload[0] == '0'){
-      //start = 0;
-      Wire.beginTransmission(I2CAddressESPWifi);
-      Wire.write(0);
-      Wire.endTransmission();
-      delay(1);//Wait for Slave to calculate response.
-    }
-    else if (payload[0] == '1'){
-      //start = 1;
-      Wire.beginTransmission(I2CAddressESPWifi);
-      Wire.write(1);
-      Wire.endTransmission();
-      delay(1);//Wait for Slave to calculate response.
-    }
-  }
-
-  // Switch on the LED if an 1 was received as first character
-//  if ((char)payload[0] == '1') {
-//    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-//    // but actually the LED is on; this is because
-//    // it is acive low on the ESP-01)
-//  } else {
-//    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-//  }
-
-}
-
-void reconnect() {
+void reconnectWifi() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
@@ -199,41 +139,88 @@ void reconnect() {
       Serial.println("connected");
       
       // Once connected, publish an announcement...
-      client.publish("outTopic", "hello");
       // ... and resubscribe
-      client.subscribe("test_connection");
-      client.subscribe("test/receive_data");
-    } else {
+      client.subscribe("control");
+    } 
+    else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
-      Serial.println(" try again in 2 seconds");
-      // Wait 5 seconds before retrying
+      Serial.println("try again in 2 seconds");
+      // Wait 2 seconds before retrying
       delay(2000);
     }
   }
 }
 
+void i2cRequestData(){
+  Wire.beginTransmission(I2CAddressESPWifi);
+  Wire.write(5);
+  Wire.endTransmission();
+  delay(1);   // Wait for Arduino slave to process data
+  int dataAvailable = Wire.requestFrom(I2CAddressESPWifi, 8);
+  
+  unsigned int i = 0;
+  byte msg[8] = {0};
+  if (dataAvailable == 8){
+//    Serial.print("Request Return:[");
+    while(Wire.available()){
+      byte c = Wire.read();
+      msg[i++] = c;
+//      Serial.print(c);
+//      Serial.print(" ");
+    }
+    
+    s1 = (msg[0] << 8) | msg[1];
+//    Serial.println(s1);
+    s2 = (msg[2] << 8) | msg[3];
+//    Serial.println(s2);
+    
+    dir = msg[4];
+//    Serial.println(dir); 
+    pos = (msg[5] << 8) | msg[6];
+//    Serial.println(pos);
+//    Serial.println("]");
+    object = msg[7];
+  }
+  else {
+    Serial.println("Unexpected number of bytes receiced.");
+  } 
+  delay(500);
+}
 
-void loop() {
-  i2cConnect();
+void callback(char* topic, byte* payload, unsigned int length) {
+  if(strcmp(topic, "control") == 0){
+//    Serial.print("Message from server: ");
+//    Serial.print(topic);
+//    Serial.print("] ");
+//    for (int i = 0; i < length; i++) {
+//      Serial.print(payload[i]);
+//    }
+    if (payload[0] ){
+      //start = 0;
+      Wire.beginTransmission(I2CAddressESPWifi);
+      Wire.write(payload[0] - '0');
+      Wire.endTransmission();
+      delay(1);//Wait for Slave to calculate response.
+    }
+  }
+}
+
+void loop(){
+  // Requesting data from slave
+  i2cRequestData();
 
   if (!client.connected()) {
-    reconnect();
+    reconnectWifi();
   }
   client.loop();
 
   long now = millis();
   if (now - lastMsg > 2000) {
-    //Serial.print("Publish message: ");
-    // Get the json string
-    char *mes = encode_to_json();
-    if (client.publish("planting_room/data/module001", mes) == true){
-        Serial.println("Success");
-    }
-    else {
-      Serial.println("Error");
-    }
+
+    encodeToJSON();
+
     delay(500);
   }
-  delay(1000);
+  delay(100);
 }
--- I2C/Uno/Uno.ino
+++ I2C/Uno/Uno.ino
@@ -8,7 +8,7 @@
 // Pins for motor control
 int motorPin = 12;
 int disable = 13;
-int dirPin = 4;
+int directionPin = 4;       
 
 // Pin number for the activation button
 int button = 2;
@@ -25,8 +25,9 @@ bool grasped = false; // indicator if the hand is running
 // #### PARAMETERS ####
 // Min and max and position of motor (unused for now)
 int min = 0;
-int max = 100;
+int max = 10000;
 unsigned int pos = 0;
+unsigned int calibratedPos = 3000;
 
 // Step size and delay. Increase step and/or lower delay for faster movement.
 int step = 10;
@@ -34,13 +35,15 @@ int wait = 2;
 bool calibrated = false;
 
 // Pressure sensor threshold (in newtons)
-double pressureThreshold = 750;
+double pressureThreshold = 500;
 bool dir = false;
 bool closed = false;      // for tracking if claw is now closed or open.
 bool idleState = false;   // The piston is at an offset position from the bottom.
 bool running = false;
-byte start = 0;
+
 byte data[8];
+
+volatile byte control = 4;  // volatile type to force the program to read the memory otherwise, some delay is required
 
 
 unsigned int sen1 = 0;
@@ -56,42 +59,52 @@ void setup() {
 
   // Set pinmodes for led, button and sensors
   pinMode(button, INPUT_PULLUP);
-  pinMode(dirPin, OUTPUT);
+  pinMode(directionPin, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(sensor1, INPUT);
   pinMode(sensor2, INPUT);
   pinMode(endstop, INPUT_PULLUP);
   pinMode(IRsensor, INPUT);
   pinMode(disable, OUTPUT);
+  pinMode(motorPin, OUTPUT);
   calibrate();
   calibrated = true;
   digitalWrite(disable, true);
+  //pinMode(9, OUTPUT);
+  //digitalWrite(9, HIGH);
 }
 
 
 // Message handling from esp8266
 void espWifiReceiveEvent(int count)
 {
+  /* Receive controlling commands
+   *  0 - stop the hand
+   *  1 - close the hand
+   *  2 - open the hand
+   *  3 - crush the object
+   *  4 - automatic control
+  */
   Serial.print("Control [");
   byte c;
   while (Wire.available())
   {
     c = Wire.read();
-    Serial.print(c);
+    //Serial.print(c);
+  }
+  
+  if (c == 5){    //
+    serializeData();
+  }
+  else {
+    control = c;
+    Serial.print(control);
   }
   Serial.println("]");
-  if (c == 0){
-    running = false;
-  }
-  else if (c == 1){
-    running = true;
-  }
-  else if (c == 2){
-    convertData();
-  }
 }
 
-void convertData(){
+void serializeData(){
+  
   int sen1 = analogRead(sensor1);
   int sen2 = analogRead(sensor2);
   data[0] = (sen1 >> 8) & 0xFF;
@@ -111,26 +124,25 @@ void convertData(){
   data[5] = (pos >> 8) & 0xFF;
   data[6] = pos & 0xFF;
   data[7] = readIRSensor() & 0xFF;
-  Serial.print(sen1);
-  Serial.print(" ");
-  Serial.print((data[0] << 8) | data[1]);
-  Serial.print("  ");
-  Serial.print(sen2);
-  Serial.print(" ");
-  Serial.print((data[2] << 8) | data[3]);
-  Serial.print("  ");
-  Serial.print((data[4]));
-  Serial.print("  ");
-  Serial.print(pos);
-  Serial.print(" ");
-  Serial.println((data[5] << 8) | data[6]);
+//  Serial.print(sen1);
+//  Serial.print(" ");
+//  Serial.print((data[0] << 8) | data[1]);
+//  Serial.print("  ");
+//  Serial.print(sen2);
+//  Serial.print(" ");
+//  Serial.print((data[2] << 8) | data[3]);
+//  Serial.print("  ");
+//  Serial.print((data[4]));
+//  Serial.print("  ");
+//  Serial.print(pos);
+//  Serial.print(" ");
+//  Serial.println((data[5] << 8) | data[6]);
 }
 
 void espWifiRequestEvent()
 {
-
-  Wire.write(data, 7);
-   
+  // Send information from hand controller to ESP
+  Wire.write(data, 8);
 }
 
 // Function for reading the pressure sensors' values.
@@ -141,15 +153,12 @@ bool readPressureSensors() {
     switch (i) {
       case 1:
         value = analogRead(sensor1);
-
-        //       Serial.print("Sensor value: "+ String(value)+" Pos: "+String(pos)+"\n");
+        //Serial.print("Sensor value: "+ String(value)+" Pos: "+String(pos)+"\n");
         break;
       case 2:
         value = analogRead(sensor2);
-
         break;
-    }
-    // TODO: Do the required math/magic to change read value to something sensible
+    }   
     //    Serial.print("Sensor "+String(i)+" value: "+String(value)+"\n");
     if (value > pressureThreshold) {
       Serial.print("Feedback over threshold! Sensor: " + String(i) + ", Value: " + String(value) + "\n");
@@ -181,7 +190,7 @@ bool feedback() {
     grasped = 1;
     return false;
   }
-  else if (!readEndStops()) { // TODO: add calls to other reading functions to statement as more sensors are added
+  else if (!readEndStops()) {
     return false;
   }
   return true;
@@ -189,30 +198,31 @@ bool feedback() {
 
 // set stepper to (global variable pos)
 void move() {
-
-  if (dir && calibrated && pos < 3000) {
+  digitalWrite(disable,false);
+  running = true;
+  if (dir && calibrated && pos < calibratedPos) {
     // The piston run to offset position
     idleState = false;
     pos += step;
-    digitalWrite(dirPin, HIGH);
+    digitalWrite(directionPin, HIGH);
   }
-  else if (dir && calibrated && pos >= 3000 && pos < 3005) {
+  else if (dir && calibrated && pos == calibratedPos) {
     // The piston stop at the offset position
     idleState = true;
     pos += step;
   }
-  else if (dir && calibrated && pos > 3005) {
+  else if (dir && calibrated && pos > calibratedPos) {
     // The piston runs to grasp the object
     idleState = false;
     pos += step;
-    digitalWrite(dirPin, HIGH);
+    digitalWrite(directionPin, HIGH);
   }
   else if (!dir && !calibrated) {
     // The piston runs calibration
     idleState = false;
     grasped = false;
     pos -= step;
-    digitalWrite(dirPin, LOW);
+    digitalWrite(directionPin, LOW);
   }
   else if (!dir && calibrated) {
     // The piston runs to the offset position
@@ -220,7 +230,7 @@ void move() {
       idleState = false;
       grasped = false;
       pos -= step;
-      digitalWrite(dirPin, LOW);
+      digitalWrite(directionPin, LOW);
     }
     else {
       digitalWrite(disable, true);
@@ -228,75 +238,119 @@ void move() {
       grasped = false;
     }
   }
-
+  
   for (int i = 0; i <= step ; i++) {
+    // Run the motor
     digitalWrite(motorPin, HIGH);
-
     delay(wait);
-
     digitalWrite(motorPin, LOW);
   }
 }
 
-void calibrate() {
+void calibrate() {  
+  // Calibrating the system, stop when reaching the endstop
   dir = false;
   while (readEndStops()) {
-    running = true;
+    Serial.println("calibrating");
     move();
   }
+  digitalWrite(disable, true);
   running = false;
   delay(1000);
   pos = 0;
 }
 
+bool checkpos(){
+  // Check whether the maximum position is reached
+  if(pos < max){
+    return true;
+  }
+  //grasped = true;
+  return false;
+}
+
 // Control loop
 void loop() {
-  
 
-  // Detecting object
-  if (readIRSensor()) {
-    Serial.print("IR ");
-    Serial.print(readIRSensor());
-
+  if (control == 1){   // close the hand command from server
     dir = 1;
-
-    // If an object presnts, the piston closes until at maximum position or until feedback says to stop
-    while (feedback() && readIRSensor() && !grasped) {
-      
-      idleState = false;
-      running = true;
-      digitalWrite(disable, false);
+    //digitalWrite(disable, false);
+    while (feedback() && checkpos() && control == 1){
+      //Serial.print("");//Why does this make it work?
       move();
     }
+  }
+  else if(control == 2){    // open the hand command from server
+    dir = 0;    
+    while (!idleState && control == 2){
+      //Serial.print("");//Why does this make it work?
+      move();
+    }
+  }
+  else if (control == 3){       // crush stuff
+    dir = 1;
+    while(checkpos() && control == 3){
+      move();
+    }
+  }
+  else if (control == 4) {  // Self control 
+    // Detecting object
+    if(readIRSensor()){
+//      Serial.print("IR ");
+//      Serial.print(readirectionPinSensor());
+      
+      dir = 1;
+      
+      // If an object presnts, the piston closes until at maximum position or until feedback says to stop
+      while(feedback() && readIRSensor() && !grasped && checkpos() && control == 4){
+        idleState = false;
+        move();
+      }
 
-    // When the object is already grasped, press the button to release the fingers.
-    if (digitalRead(button) == LOW && grasped) {
-      digitalWrite(disable, false);
-      dir = 0;
-      while (!idleState) {
-        running = true;
+      // When the object is already grasped, press the button to release the fingers.
+      if (digitalRead(button) == LOW && grasped){
+
+        // CRUSH CODE
+        for(int i = 0; !digitalRead(button) ; i++){
+          delay(50);
+          if(i > 20){
+            Serial.print("Would you kindly release the button to...\n");
+            Serial.print("CRUSH, MAIM, DESTROY!\n");
+            while(!digitalRead(button)){ /* DO NOTHING */}
+            //digitalWrite(disable,false);
+            Serial.print("Crushing...\n");
+            while(checkpos() && digitalRead(button) && control == 4){
+              move();
+            }
+            Serial.print("Object crushed!\n");
+            while(digitalRead(button)){ /* DO NOTHING */}
+            break;
+          }
+        }
+        // END OF CRUSH CODE
+     
+        directionPin = 0;
+        while(!idleState && control == 4){
+          move();
+        }
+      }
+    }
+    else {
+      // Change the directionPinectionPin to move to offset position
+      if (!dir){  
+        dir = 1;
+      }
+      else if (dir){ 
+        dir = 0;
+      }
+      
+      // Move the piston to the offset position when there is no object or when the button is pressed to release the fingers
+      while(!readIRSensor() && !idleState && control == 4){
         move();
       }
     }
-    digitalWrite(disable, true);
-    running = false;
   }
-  else {
-    // Change the direction to move to offset position
-    if (!dir) {
-      dir = 1;
-    }
-    else if (dir) {
-      dir = 0;
-    }
-    digitalWrite(disable, false);
-
-    // Move the piston to the offset position when there is no object or when the button is pressed to release the fingers
-    while (!readIRSensor() && !idleState) {
-      running = true;
-      move();
-    }
-    digitalWrite(disable, true);
-    running = false;
-  }
+  
+  digitalWrite(disable, true);
+  running = false;
 }
